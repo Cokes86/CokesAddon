235e28491785c26d7e75ef69b77f13b78cca14d5
Cokes86 <sgl01089639576@gmail.com>
2020-06-22 13:43:01 +0900

    VigilanteLeader Bug Fixed / Block_Obsidian damage knockback source remake / Source Cleanup

--- src/cokes86/addon/ability/list/Thorn.java
+++ src/cokes86/addon/ability/list/Thorn.java
@@ -18,41 +18,33 @@ import daybreak.abilitywar.ability.decorator.ActiveHandler;
 import daybreak.abilitywar.game.AbstractGame.Participant;
 import daybreak.abilitywar.utils.library.SoundLib;
 
-@AbilityManifest(
-		name = "가시",
-		rank = Rank.A,
-		species = Species.OTHERS,
-		explain = {"철괴 우클릭시 $[du]동안 상대방의 화살 공격의 대미지를 0으로 하고,",
-		"무시한 대미지의 $[damage]%만큼을 상대방에게 되돌려줍니다. $[cool]"}
-)
+@AbilityManifest(name = "가시", rank = Rank.A, species = Species.OTHERS, explain = {
+		"철괴 우클릭시 $[du]동안 상대방의 화살 공격의 대미지를 0으로 하고,", "무시한 대미지의 $[damage]%만큼을 상대방에게 되돌려줍니다. $[cool]" })
 public class Thorn extends AbilityBase implements ActiveHandler {
 	private static Config<Integer> cool = new Config<Integer>(Thorn.class, "쿨타임", 20, 1) {
 		public boolean Condition(Integer value) {
 			return value >= 0;
 		}
-	},
-			du = new Config<Integer>(Thorn.class, "지속시간",5 ,2) {
-				public boolean Condition(Integer value) {
-					return value >= 0;
-				}
-			},
-			damage = new Config<Integer>(Thorn.class, "반사대미지(%)", 20) {
-				public boolean Condition(Integer value) {
-					return value > 0;
-				}
-			};
+	}, du = new Config<Integer>(Thorn.class, "지속시간", 5, 2) {
+		public boolean Condition(Integer value) {
+			return value >= 0;
+		}
+	}, damage = new Config<Integer>(Thorn.class, "반사대미지(%)", 20) {
+		public boolean Condition(Integer value) {
+			return value > 0;
+		}
+	};
 
 	public Thorn(Participant participant) throws IllegalStateException {
 		super(participant);
 	}
-	
+
 	CooldownTimer c = new CooldownTimer(cool.getValue());
 	DurationTimer d = new DurationTimer(du.getValue(), c) {
 		@Override
 		protected void onDurationProcess(int arg0) {
 		}
 	};
-	
 
 	@Override
 	public boolean ActiveSkill(Material mt, ClickType ct) {
@@ -64,7 +56,7 @@ public class Thorn extends AbilityBase implements ActiveHandler {
 		}
 		return false;
 	}
-	
+
 	@SubscribeEvent
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
 		if (e.getEntity().equals(getPlayer()) && d.isRunning()) {
@@ -73,7 +65,7 @@ public class Thorn extends AbilityBase implements ActiveHandler {
 				if (a.getShooter() instanceof Player) {
 					Player shooter = (Player) a.getShooter();
 					double dam = e.getFinalDamage();
-					DamagePlusUtil.penetratingDamage(dam* damage.getValue() / (float)100, shooter, getPlayer());
+					DamagePlusUtil.penetratingDamage(dam * damage.getValue() / (float) 100, shooter, getPlayer());
 					SoundLib.PIANO.playInstrument(shooter, new Note(1, Tone.F, false));
 					e.setDamage(0);
 				}
--- src/cokes86/addon/ability/list/VigilanteLeader.java
+++ src/cokes86/addon/ability/list/VigilanteLeader.java
@@ -24,29 +24,23 @@ import daybreak.abilitywar.utils.base.math.geometry.Circle;
 import daybreak.abilitywar.utils.library.ParticleLib;
 import daybreak.abilitywar.utils.library.ParticleLib.RGB;
 
-@AbilityManifest(
-		name = "자경단장",
-		rank = AbilityManifest.Rank.B,
-		species = AbilityManifest.Species.HUMAN,
-		explain = {"게임 중 최초로 철괴 우클릭 시 범위 $[r]블럭 이내 모든 범위를 자경단 아지트로 만듭니다.",
-		"또한 해당 범위 안에 있던 플레이어 전부 자경단원으로 소속됩니다.",
-		"이후 철괴 우클릭 시 힘1 버프를 (참가자의 수*0.5)초 부여합니다. $[cool]",
-		"자경단 아지트 내에서 참가자는 2명, 자경단원은 3명, 자경단장은 4명 취급하며",
-		"자경단 아지트 밖에서 자경단장은 2명 취급합니다."}
-)
+@AbilityManifest(name = "자경단장", rank = AbilityManifest.Rank.B, species = AbilityManifest.Species.HUMAN, explain = {
+		"게임 중 최초로 철괴 우클릭 시 범위 $[r]블럭 이내 모든 범위를 자경단 아지트로 만듭니다.", "또한 해당 범위 안에 있던 플레이어 전부 자경단원으로 소속됩니다.",
+		"이후 철괴 우클릭 시 힘1 버프를 (참가자의 수*0.5)초 부여합니다. $[cool]", "자경단 아지트 내에서 참가자는 2명, 자경단원은 3명, 자경단장은 4명 취급하며",
+		"자경단 아지트 밖에서 자경단장은 2명 취급합니다." })
 public class VigilanteLeader extends AbilityBase implements ActiveHandler {
 	public static Config<Integer> r = new Config<Integer>(VigilanteLeader.class, "아지트범위", 10) {
 		@Override
 		public boolean Condition(Integer value) {
 			return value > 0;
 		}
-	},
-			cool = new Config<Integer>(VigilanteLeader.class, "쿨타임", 90,1) {
-				@Override
-				public boolean Condition(Integer value) {
-					return value >= 0;
-				}
-			};
+	}, cool = new Config<Integer>(VigilanteLeader.class, "쿨타임", 90, 1) {
+		@Override
+		public boolean Condition(Integer value) {
+			return value >= 0;
+		}
+	};
+	RGB color = new RGB(0, 162, 232);
 	ArrayList<Participant> vigilantes = new ArrayList<>();
 	Location ajit = null;
 	int addNum;
@@ -55,30 +49,35 @@ public class VigilanteLeader extends AbilityBase implements ActiveHandler {
 	public VigilanteLeader(Participant participant) {
 		super(participant);
 	}
-	
+
 	CooldownTimer c = new CooldownTimer(cool.getValue());
 	Timer passive = new Timer() {
 		@Override
 		protected void run(int count) {
 			if (ajit != null) {
-				for (Location l : Circle.iteratorOf(ajit, r.getValue(), r.getValue()*8).iterable()) {
-					l.setY(LocationUtil.getFloorYAt(l.getWorld(), l.getY(), l.getBlockX(), l.getBlockZ())+0.1);
-					for (Participant p : vigilantes) ParticleLib.REDSTONE.spawnParticle(p.getPlayer(), l, new RGB(0,162,232));
-					ParticleLib.REDSTONE.spawnParticle(getPlayer(), l, new RGB(0,162,232));
+				for (Location l : Circle.of(r.getValue(), r.getValue() * 8).toLocations(ajit).floor(ajit.getY())) {
+					for (Participant p : vigilantes)
+						ParticleLib.REDSTONE.spawnParticle(p.getPlayer(), l, color);
+					ParticleLib.REDSTONE.spawnParticle(getPlayer(), l, color);
 				}
-				
+
 				ArrayList<Player> inAjit = LocationUtil.getNearbyPlayers(ajit, r.getValue(), r.getValue());
 				addNum = inAjit.size();
-				
-				if (inAjit.contains(getPlayer())) {addNum +=2;} else {addNum += 1;}
-				for (Participant vigilante : vigilantes) {
-					if (inAjit.contains(vigilante.getPlayer())) addNum+=1;
+
+				if (inAjit.contains(getPlayer())) {
+					addNum += 2;
+				} else {
+					addNum += 1;
 				}
-				
-				channel.update("인원 수: "+getGame().getParticipants().size()+"+"+addNum+"");
+				for (Participant vigilante : vigilantes) {
+					if (inAjit.contains(vigilante.getPlayer()))
+						addNum += 1;
+				}
+
+				channel.update("인원 수: " + getGame().getParticipants().size() + "+" + addNum + "");
 			}
 		}
-	}.setPeriod(TimeUnit.TICKS, 1);
+	}.setPeriod(TimeUnit.TICKS, 2);
 
 	@Override
 	public boolean ActiveSkill(Material m, ClickType ct) {
@@ -89,15 +88,16 @@ public class VigilanteLeader extends AbilityBase implements ActiveHandler {
 				for (Player p : LocationUtil.getNearbyPlayers(ajit, r.getValue(), r.getValue())) {
 					if (getGame().isParticipating(p) && !p.equals(getPlayer())) {
 						vigilantes.add(getGame().getParticipant(p));
-						getPlayer().sendMessage(p.getName()+"을 자경단원으로 영입했습니다.");
-						p.sendMessage("당신은 "+getPlayer().getName()+"의 자경단에 입단하였습니다.");
+						getPlayer().sendMessage(p.getName() + "을 자경단원으로 영입했습니다.");
+						p.sendMessage("당신은 " + getPlayer().getName() + "의 자경단에 입단하였습니다.");
 					}
 				}
 				return true;
 			} else {
 				if (!c.isCooldown()) {
-					getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, (getGame().getParticipants().size()+addNum)*10, 0));
-					getPlayer().sendMessage((getGame().getParticipants().size()+addNum)*0.5+"초의 힘버프가 제공되었습니다.");
+					getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,
+							(getGame().getParticipants().size() + addNum) * 10, 0));
+					getPlayer().sendMessage((getGame().getParticipants().size() + addNum) * 0.5 + "초의 힘버프가 제공되었습니다.");
 					c.start();
 					return true;
 				}
@@ -105,18 +105,26 @@ public class VigilanteLeader extends AbilityBase implements ActiveHandler {
 		}
 		return false;
 	}
-	
+
 	@SubscribeEvent(onlyRelevant = true)
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
 		if (e.getEntity().equals(getPlayer()) && e.getDamager() instanceof Player) {
 			Player t = (Player) e.getDamager();
-				AbstractGame g = getGame();
-				if (g.isParticipating(t) && vigilantes.contains(g.getParticipant(t))) {
-					e.setDamage(e.getDamage() * 7 / 10);
-				}
+			AbstractGame g = getGame();
+			if (g.isParticipating(t) && vigilantes.contains(g.getParticipant(t))) {
+				e.setDamage(e.getDamage() * 7 / 10);
+			}
 		}
 	}
-	
+
+	public void onUpdate(Update update) {
+		switch (update) {
+		case RESTRICTION_CLEAR:
+			passive.start();
+		default:
+		}
+	}
+
 	@SubscribeEvent(onlyRelevant = true)
 	public void onRestrictionClear(AbilityRestrictionClearEvent e) {
 		passive.start();
