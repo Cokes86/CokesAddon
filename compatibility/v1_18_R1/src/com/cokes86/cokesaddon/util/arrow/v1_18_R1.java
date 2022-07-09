05539b1ee3e1c4ae797b9dd475767585d5dacf33
Cokes86 <sgl01089639576@gmail.com>
2022-07-10 00:37:00 +0900

    1.9.0 DEV_1
    1.8 ~ 1.9에 대한 임시 호환작업 [추후 업데이트 대비]

---
+++ compatibility/v1_18_R1/src/com/cokes86/cokesaddon/util/arrow/v1_18_R1.java
@@ -0,0 +1,43 @@
+package com.cokes86.cokesaddon.util.arrow;
+
+import org.bukkit.entity.AbstractArrow;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+
+public class v1_18_R1 implements ArrowImpl{
+    @Override
+    public void setCritical(Entity arrow, boolean critical) {
+        if (!(arrow instanceof AbstractArrow)) throw new IllegalArgumentException("arrow must be an instance of Arrow");
+        ((Arrow) arrow).setCritical(critical);
+    }
+
+    @Override
+    public boolean isCritical(Entity arrow) {
+        if (!(arrow instanceof AbstractArrow)) throw new IllegalArgumentException("arrow must be an instance of Arrow");
+        return ((Arrow) arrow).isCritical();
+    }
+
+    @Override
+    public int getPierceLevel(Entity arrow) {
+        if (!(arrow instanceof AbstractArrow)) throw new IllegalArgumentException("arrow must be an instance of Arrow");
+        return ((AbstractArrow) arrow).getPierceLevel();
+    }
+
+    @Override
+    public void setPierceLevel(Entity arrow, int level) {
+        if (!(arrow instanceof AbstractArrow)) throw new IllegalArgumentException("arrow must be an instance of Arrow");
+        ((AbstractArrow) arrow).setPierceLevel(level);
+    }
+
+    @Override
+    public void setShotFromCrossbow(Entity arrow, boolean shotFromCrossbow) {
+        if (!(arrow instanceof AbstractArrow)) throw new IllegalArgumentException("arrow must be an instance of Arrow");
+        ((AbstractArrow) arrow).setShotFromCrossbow(shotFromCrossbow);
+    }
+
+    @Override
+    public boolean isShotFromCrossbow(Entity arrow) {
+        if (!(arrow instanceof AbstractArrow)) throw new IllegalArgumentException("arrow must be an instance of Arrow");
+        return ((AbstractArrow) arrow).isShotFromCrossbow();
+    }
+}
---
+++ compatibility/v1_18_R1/src/com/cokes86/cokesaddon/util/damage/v1_18_R1.java
@@ -0,0 +1,58 @@
+package com.cokes86.cokesaddon.util.damage;
+
+import net.minecraft.server.level.EntityPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.EntityDamageSourceIndirect;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.projectile.EntityPotion;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class v1_18_R1 implements DamageImpl{
+    private static final ItemStack SPLASH_POTION = new ItemStack(Items.sr);
+
+    @Override
+    public boolean damageMagicFixed(@NotNull org.bukkit.entity.Entity entity, @Nullable Player damager, float damage) {
+        Entity nmsEntity = ((CraftEntity)entity).getHandle();
+        EntityPlayer nmsDamager = damager != null ? ((CraftPlayer)damager).getHandle() : null;
+
+        return nmsEntity.a(magic(nmsEntity, nmsDamager), damage);
+    }
+
+    @Override
+    public boolean damageWither(@NotNull org.bukkit.entity.Entity entity, float damage) {
+        Entity nmsEntity = ((CraftEntity)entity).getHandle();
+        return nmsEntity.a(DamageSource.p, damage);
+    }
+
+    @Override
+    public boolean damageVoid(org.bukkit.entity.@NotNull Entity entity, @Nullable Player damager, float damage) {
+        Entity nmsEntity = ((CraftEntity)entity).getHandle();
+        EntityPlayer nmsDamager = damager != null ? ((CraftPlayer)damager).getHandle() : null;
+        return nmsEntity.a(DamageSource.m, damage);
+    }
+
+    private EntityDamageSourceIndirect magic(Entity nmsEntity, EntityPlayer nmsDamager) {
+        EntityDamageSourceIndirect source;
+        if (nmsDamager != null) {
+            source = new EntityDamageSourceIndirect("magic", setItem(new EntityPotion(nmsEntity.t, nmsDamager)), nmsDamager) {{
+                y(); m(); o();
+            }};
+        } else {
+            source = new EntityDamageSourceIndirect("magic", setItem(new EntityPotion(nmsEntity.t, nmsEntity.dc(), nmsEntity.de(), nmsEntity.di())), null){{
+                y(); m(); o();
+            }};
+        }
+        return source;
+    }
+
+    private EntityPotion setItem(final EntityPotion potion) {
+        potion.a(SPLASH_POTION);
+        return potion;
+    }
+}
